659ad3f40ddd0d320a5ecacc195c666738b4c5be
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-24 17:32:19 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -83,7 +83,7 @@ async def ping(ctx):
       t1 = time.perf_counter()
       await client.send_typing(channel)
       t2 = time.perf_counter()
-      await client.say(ctx.message.author.mention + "Pong: {}ms".format(round((t2-t1)*2000)))
+      await client.say("Pong: {}ms".format(round((t2-t1)*2000 + ctx.message.author.mention +)))
 
 @client.command(pass_context=True)
 async def jointest(ctx):
