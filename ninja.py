b4a0a72ad19f1697c138f27de5f51ae9f9d6e916
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-26 21:30:13 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1290,10 +1290,6 @@ async def movie(ctx, *, name:str=None):
             embed.set_footer(text = "Information from the OMDB API")
             await client.say(embed=embed)
     
-@client.event
-async def on_message(message):
-    if message.content.startswith('@Official NinjaBot™'):
-        await client.say('My prefix is n! or mention me')
 
             
 
