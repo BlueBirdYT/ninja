3a1cc9e5bc3597ca8d4819f8d859bbfda98594da
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-26 20:36:42 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -51,7 +51,7 @@ async def on_ready():
 async def on_reaction_add(reaction, user):
  if reaction.message.server is None:
      if reaction.emoji == '🇬':
-               r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
     embed.set_author(name='Help')
     embed.set_image(url = 'https://image.ibb.co/caM2BK/help.gif')
