22395849e816bc5f6862ed9058f2d231499af24e
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-28 18:09:56 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1270,7 +1270,12 @@ async def movie(ctx, *, name:str=None):
             embed.set_footer(text = "Information from the OMDB API")
             await client.say(embed=embed)
  
-
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+     if '<@>' in message.content:
+          msg = '**my prefix is n!, Use ``n!help`` for more information**'.format(message)
+          msg2 = await client.send_message(message.channel, msg)
 
             
     
