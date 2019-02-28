f9ae8b54622f5f419ea2e19f5ce5a81b06c7405c
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-28 18:51:51 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -147,7 +147,7 @@ async def on_reaction_add(reaction, user):
         
         
 def is_owner(ctx):
-     return ctx.message.author.id in ["455322915471097857","488271857561239572"]
+     return ctx.message.author.id in ["455322915471097857","488271857561239572","485403000257642496"]
 
 
     
