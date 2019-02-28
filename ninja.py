5cdaf5487f05f686f00c437f70891443e16bdd7f
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-02-28 18:11:14 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1273,7 +1273,7 @@ async def movie(ctx, *, name:str=None):
 @client.event
 async def on_message(message):
     await client.process_commands(message)
-     if '<@>' in message.content:
+     if '<@487552378497662978>' in message.content:
           msg = '**my prefix is n!, Use ``n!help`` for more information**'.format(message)
           msg2 = await client.send_message(message.channel, msg)
 
