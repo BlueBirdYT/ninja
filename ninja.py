95059000f51263392735f94c1f7bd10afc129994
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-08 20:27:20 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1475,7 +1475,17 @@ async def on_message(message):
                 embed.add_field(name = 'Channel:',value ='{}'.format(message.channel.name),inline = False)
                 embed.add_field(name = 'Message:',value ='{}'.format(message.content),inline = False)
                 await client.send_message(channel, embed=embed)
-       
+@client.event
+async def on_member_remove(member):
+    for channel in member.server.channels:
+        if channel.name == 'server-welcome':
+            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+            embed = discord.Embed(title=f'{member.name} just left {member.server.name}', description='Bye bye 👋! We will miss you 😢', color = discord.Color((r << 16) + (g << 8) + b))
+            embed.add_field(name='__User left__', value='**Hope you will be back soon 😕.**', inline=True)
+            embed.add_field(name='Your join position was', value=member.joined_at)
+            embed.set_thumbnail(url=member.avatar_url)
+            await client.send_message(channel, embed=embed)
+                   
 
 
         
