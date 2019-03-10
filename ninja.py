4f63737e2ef83d92a4835f47139faf5f7d0ce8f2
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-10 22:25:33 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -22,9 +22,9 @@ GIPHY_API_KEY = "dc6zaTOxFJmzC"
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(name=' n!help',status=discord.Status.do_not_disturb,type=2))
+        await client.change_presence(game=discord.Game(name=' n!help',type=2))
         await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='Music',status=discord.Status.do_not_disturb))
+        await client.change_presence(game=discord.Game(name='Music'))
         await asyncio.sleep(4)
         await client.change_presence(game=discord.Game(name=str(len(set(client.get_all_members())))+' users',status=discord.Status.do_not_disturb,type=3))
         await asyncio.sleep(4)
