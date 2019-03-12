0d1d0fd3a2e48abb7655a5a0bce9fd619a38f8a0
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-12 21:12:00 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -22,17 +22,17 @@ GIPHY_API_KEY = "dc6zaTOxFJmzC"
 
 async def status_task():
     while True:
-        await client.change_presence(status=discord.Status.dnd, game=discord.Game(name=' n!help',type=2))
+        await client.change_presence(status=discord.Status.idle, game=discord.Game(name=' n!help',type=2))
         await asyncio.sleep(5)
         await client.change_presence(status=discord.Status.dnd, game=discord.Game(name='Music'))
         await asyncio.sleep(4)
-        await client.change_presence(status=discord.Status.dnd, game=discord.Game(name=str(len(set(client.get_all_members())))+' users',type=3))
+        await client.change_presence(status=discord.Status.idle, game=discord.Game(name=str(len(set(client.get_all_members())))+' users',type=3))
         await asyncio.sleep(4)
-        await client.change_presence(status=discord.Status.dnd, game=discord.Game(name='xp system added type n!ownerinfo for more informatiion'))
+        await client.change_presence(status=discord.Status.idle, game=discord.Game(name='xp system added type n!ownerinfo for more informatiion'))
         await asyncio.sleep(4)
         await client.change_presence(status=discord.Status.dnd, game=discord.Game(name=str(len(client.servers))+' servers',type=3))
         await asyncio.sleep(4)
-        await client.change_presence(status=discord.Status.dnd, game=discord.Game(name='i need upvotes pls vote by typing n!Help'))
+        await client.change_presence(status=discord.Status.idle, game=discord.Game(name='i need upvotes pls vote by typing n!Help'))
         await asyncio.sleep(4)
        
 
