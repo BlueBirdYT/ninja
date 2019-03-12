2301d954de9eb866d695bf6fce56639491b0bdf8
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-12 14:57:55 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1284,7 +1284,7 @@ async def movie(ctx, *, name:str=None):
         r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
         await client.send_typing(ctx.message.channel)
         if name is None:
-                embed=discord.Embed(description = "Please specify a movie, *eg. mv!movie Inception*", color = discord.Color((r << 16) + (g << 8) + b))
+                embed=discord.Embed(description = "Please specify a movie, *eg. n!movie avengers*", color = discord.Color((r << 16) + (g << 8) + b))
                 x = await client.say(embed=embed)
                 await asyncio.sleep(5)
                 return await client.delete_message(x)
