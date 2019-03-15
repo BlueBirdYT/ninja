5682c1be1c97499fcfac2637cd7caae4af998ef3
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-15 13:24:54 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -10,8 +10,38 @@ import os
 import requests
 import datetime
 import json
+import dbl
+import logging
 from discord.voice_client import VoiceClient
 from discord import Game, Embed, Color, Status, ChannelType
+
+class DiscordBotsOrgAPI:
+    """Handles interactions with the discordbots.org API"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4NzU1MjM3ODQ5NzY2Mjk3OCIsImJvdCI6dHJ1ZSwiaWF0IjoxNTUyMTUwMjg1fQ.lDR5YXyWkm8azpXfoURVsJara38s0NcCqPGOhxehaIM
+'  #  set this to your DBL token
+        self.dblpy = dbl.Client(self.bot, self.token)
+        self.bot.loop.create_task(self.update_stats())
+
+    async def update_stats(self):
+        """This function runs every 30 minutes to automatically update your server count"""
+
+        while True:
+            logger.info('attempting to post server count')
+            try:
+                await self.dblpy.post_server_count()
+                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+            except Exception as e:
+                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            await asyncio.sleep(1800)
+
+def setup(bot):
+    global logger
+    logger = logging.getLogger('bot')
+    bot.add_cog(DiscordBotsOrgAPI(bot))
+
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
