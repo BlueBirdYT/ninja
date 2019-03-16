2befd6e34d509e525118eed757956efe4f8b4263
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-16 12:42:16 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1720,7 +1720,7 @@ async def howgay(ctx, user: discord.Member = None):
     embed = discord.Embed(title=f"Gay checking cellphone", description=f"{user} is **{score}%** gay :rainbow:", color = discord.Color((r << 16) + (g << 8) + b))
     await client.say(embed=embed)    
     
-@bot.command(pass_context=True)
+@client.command(pass_context=True)
 async def love(ctx, user: discord.Member = None, *, user2: discord.Member = None):
     shipuser1 = user.name
     shipuser2 = user2.name
@@ -1743,7 +1743,7 @@ async def love(ctx, user: discord.Member = None, *, user2: discord.Member = None
             r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
             embed = discord.Embed(title=f"{shipuser1} ❤ {shipuser2} Love each others", description=f"Love\n`{counter_}` Score:**{score}% **\nLoveName:**{finalName}**", color = discord.Color((r << 16) + (g << 8) + b))
             embed.set_image(url=res['message'])
-            await bot.say(embed=embed)   		   	   	    
+            await client.say(embed=embed)   		   	   	    
     
     
 client.run(os.getenv('Token'))
