b47bd044004de8a932ffa5e94700c62666bac86d
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-03-22 17:53:03 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1707,28 +1707,7 @@ async def love(ctx, user: discord.Member = None, *, user2: discord.Member = None
             embed.set_image(url=res['message'])
             await client.say(embed=embed)   		   	   	    
 
-@client.event
-async def on_server_join(server):
-		if channel.name == 'server-joins-ninja':
-			logchannel = channel
-			r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-			embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-			embed.set_author(name='IM IN A NEW SERVER')
-			embed.add_field(name = 'Server Name:',value ='{}'.format(guild.name),inline = False)
-			embed.add_field(name = 'Membercount',value ='str(len(guild.members))',inline = False)
-			embed.set_footer(text ='Made with ❤ by @ADIB HOQUE#6969')
-			await client.send_message(logchannel,  embed=embed)		
-			
-@client.event
-async def on_server_remove(server):
-		if channel.name == 'server-joins-ninja':
-			logchannel = channel
-			r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-			embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-			embed.set_author(name='I WAS REMOVED FROM A SERVER')
-			embed.add_field(name = 'Server Name:',value ='{}'.format(guild.name),inline = False)
-			embed.add_field(name = 'Membercount',value ='str(len(guild.members))',inline = False)
-			await client.send_message(logchannel,  embed=embed)        
+ 
             
             
     
