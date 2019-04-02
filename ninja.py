0776c5e590f28d68ecdb3a71d15771844c4e5208
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-04-02 18:01:12 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -31,7 +31,7 @@ class DiscordBotsOrgAPI:
             logger.info('attempting to post server count')
             try:
                 await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+                logger.info('posted server count ({})'.format(len(self.client.guilds)))
             except Exception as e:
                 logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
             await asyncio.sleep(1800)
@@ -1729,24 +1729,24 @@ async def love(ctx, user: discord.Member = None, *, user2: discord.Member = None
             await client.say(embed=embed)   		   	   	    
 
 @client.event
-async def on_guild_join(guild):
+async def on_server_join(server):
 	channel = client.get_channel('562605168521379870')
 	r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
 	embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
 	embed.set_author(name='IM IN A NEW SERVER')
-	embed.add_field(name = 'Server Name:',value ='{}'.format(guild.name),inline = False)
-	embed.add_field(name = 'Membercount',value ='str(len(guild.members))',inline = False)
-	await client.send_message(channel,  embed=embed)		
+	embed.add_field(name = 'Server Name:',value ='{}'.format(server.name),inline = False)
+	embed.add_field(name = 'Membercount',value ='str(len(server.members))',inline = False)
+	await client.send_message(channel,embed=embed)		
 			
 @client.event
-async def on_guild_remove(guild):
+async def on_server_remove(server):
 		channel = client.get_channel('562605168521379870')
 		r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
 		embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
 		embed.set_author(name='I WAS REMOVED FROM A SERVER')
-		embed.add_field(name = 'Server Name:',value ='{}'.format(guild.name),inline = False)
-		embed.add_field(name = 'Membercount',value ='str(len(guild.members))',inline = False)
-		await client.send_message(channel,  embed=embed) 
+		embed.add_field(name = 'Server Name:',value ='{}'.format(server.name),inline = False)
+		embed.add_field(name = 'Membercount',value ='str(len(server.members))',inline = False)
+		await client.send_message(channel,embed=embed) 
             
             
     
