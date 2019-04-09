eb007b70c7b7f6890f06ec8bfda9a8ece742db51
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-04-09 17:59:17 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -177,18 +177,7 @@ async def on_reaction_add(reaction, user):
         embed.add_field(name = 'n!profile', value ='check your level',inline = False)
         embed.add_field(name = 'n!leaderboard', value ='check the leaderboard',inline = False)
         await client.send_message(user,embed=embed)    
-     for channel in user.server.channels:
-	if channel.name == 'server-log':
-            logchannel = channel
-            r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
-            embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
-            embed.set_author(name='Reaction Added')
-            embed.add_field(name = 'User: **{0}**'.format(user.name),value ='UserID: **{}**'.format(user.id),inline = False)
-            embed.add_field(name = 'Message:',value ='{}'.format(reaction.message.content),inline = False)
-            embed.add_field(name = 'Channel:',value ='{}'.format(reaction.message.channel.name),inline = False)
-            embed.add_field(name = 'Emoji:',value ='{}'.format(reaction.emoji),inline = False)
-            await client.send_message(logchannel, embed=embed)
-     
+    
      
     
    
