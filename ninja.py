03787609f196f1c2014e179f2fae767ab31eecb8
BlueBirdYT <46397900+BlueBirdYT@users.noreply.github.com>
2019-05-11 10:30:59 +0600

    Update ninja.py

--- ninja.py
+++ ninja.py
@@ -1404,7 +1404,7 @@ async def gifsearch(ctx, *keywords):
 async def on_message(message):
     await client.process_commands(message)
     if ctx.message.author.bot:
-		return
+        return
     if '<@487552378497662978>' in message.content:
         msg = '__**Hello, my prefix is ``n!``, Use ``n!help`` for more information!**__'.format(message)
         msg2 = await client.send_message(message.channel, msg)
